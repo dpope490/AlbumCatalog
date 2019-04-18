859507a1f1e50ac6ada32dcfc7a2e6df7a78d872
Pope, David <dpope@dstsystems.com>
2019-04-18 13:43:18 -0500

    new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CatalogComponent} from './components/catalog/catalog.component';
+import {AddrecordComponent} from './components/addrecord/addrecord.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CatalogComponent},
+  {path: 'add', component: AddrecordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
